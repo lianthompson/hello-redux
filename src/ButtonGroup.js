8f3104c4e306e6f59acdb70437096da0ad8dd65d
Lian Thompson <lianythompson@gmail.com>
2019-06-25 13:05:14 -0700

    add store subscribe

---
+++ src/ButtonGroup.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { store } from './store';
+import './index.css';
+import { setTechnology } from './actions';
+
+const ButtonGroup = ({ technologies }) => (
+    <div className="hello-btns">
+        {technologies.map((tech, i) => (
+            <button
+                data-tech={tech}
+                key={`btn-${i}`}
+                className="hello-btn"
+                onClick={dispatchBtnAction}
+            >
+                {tech}
+            </button>
+        ))}
+    </div>
+);
+
+function dispatchBtnAction(e) {
+    const tech = e.target.dataset.tech;
+    store.dispatch(setTechnology(tech));
+}
+
+export default ButtonGroup;
